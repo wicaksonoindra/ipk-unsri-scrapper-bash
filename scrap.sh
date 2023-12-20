b0b1a18f9b3fbb27b05058c058fe1125eb75b807
wicaksonoindra <wicaksonoindra66@gmail.com>
2023-12-21 06:33:51 +0700

    clean result

--- scrap.sh
+++ scrap.sh
@@ -4,4 +4,4 @@ user=$1
 pass=$2
 
 get_cookie=$(curl -i -d "username=$user&userpassword=$pass&id_prodi=10003" https://akademik.unsri.ac.id/portal02/login/proses_login.php | grep Set-Cookie | cut -d " " -f 2,3)
-curl -H "Cookie: $get_cookie" -d "inp_tak_history1=2023&inp_tak_history2=2024&inp_tak_history3=1" https://akademik.unsri.ac.id/portal02/module/data_akademik/khs/update.php | html2text
+curl -H "Cookie: $get_cookie" -d "inp_tak_history1=2023&inp_tak_history2=2024&inp_tak_history3=1" https://akademik.unsri.ac.id/portal02/module/data_akademik/khs/update.php | html2text | sed -n '/Home/,$p'
